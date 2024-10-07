522ba8410cfc486b567f34fa76f68dfae95c9262
MattachineGG <lunsford.matthew@gmail.com>
2024-10-06 19:44:55 -0700

    Initial commit. Basic plugin.

---
+++ WynonasBigBrownBox/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace WynonasBigBrownBox
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "WynonasBigBrownBox";
+        public const string PLUGIN_NAME = "Wynona's Big Brown Box";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
