bc94086bed1dc227bff3cd8bb929d22ef70eafff
MattachineGG <lunsford.matthew@gmail.com>
2024-10-06 20:01:19 -0700

    Add more info/set realistic version number.

---
+++ WynonasBigBrownBox/README.md
@@ -0,0 +1,3 @@
+# Wynona's Big Brown Box
+
+Plugin for TCG Card Shop Simulator that adds bigger boxes to the store.
--- WynonasBigBrownBox/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++ WynonasBigBrownBox/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -4,6 +4,6 @@ namespace WynonasBigBrownBox
     {
         public const string PLUGIN_GUID = "WynonasBigBrownBox";
         public const string PLUGIN_NAME = "Wynona's Big Brown Box";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "0.0.1";
     }
 }
