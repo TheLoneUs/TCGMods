89499da22b2552dcc20b1e4b6523067b35802e1f
MattachineGG <lunsford.matthew@gmail.com>
2024-10-06 20:06:06 -0700

    Deleting non-required files from repo.

--- WynonasBigBrownBox/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace WynonasBigBrownBox
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "WynonasBigBrownBox";
-        public const string PLUGIN_NAME = "Wynona's Big Brown Box";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
